d7e76d3932bf6999e70173b71be5882e41012792
Alexey.Ponimash <Alexey.Ponimash@billing.ru>
2017-12-05 10:47:19 +0300

    Refactored

--- newyear2018.ino
+++ newyear2018.ino
@@ -1,41 +1,237 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+#define R 9           // красный пин
+#define G 3           // зеленый пин
+#define B 5           // синий пин
 
-#define RED 9
-#define GREEN 3
-#define BLUE 5
+// задержки
+int tim = 0;
+int tim2 = 0;
 
-#define DELAY 10
+int prog = 3;        // переменная хранения номера прграммы 
 
-int cycle = 0;
-int led = RED;
-int leds[3] = {RED,GREEN,BLUE};
+int spd = 255;
+unsigned long changed = 0;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT); 
+
+void setup() {
+  pinMode(R, OUTPUT);      // пин R на выход
+  pinMode(G, OUTPUT);      // пин G на выход
+  pinMode(B, OUTPUT);      // пин B на выход
+  
+  tim  = spd / 20 +2;
+  tim2 = spd + 70;
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  led = leds[ (cycle++) % 3];
+
+void change_prog()
+{
+  if( millis()-changed > 30*1000 ){
+    prog = (++prog) % 4 + 1;
+    changed = millis();
+  }
+}
+
+void prog_1(){
+  int r, g, b;
+
+  digitalWrite(R, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(B, LOW);
+  delay(300);
+
+  for( r = 0; r < 255; r++ ){ 
+    analogWrite(R, r);
+    delay(tim);
+  }
+  for( r=255; r >= 0; r-- ){ 
+    analogWrite(R, r);
+    delay(tim);
+  }
+  delay(tim+500);
+    
+  for( r = 0; r < 255; r++ ){ 
+    analogWrite(B, r);
+    delay(tim);
+  }
+  for (r = 255; r >= 0; r--){ 
+    analogWrite(B, r);
+    delay(tim);
+  }
+  delay(tim+500);
+    
+  for( r = 0; r < 255; r++ ){ 
+    analogWrite(R, r);
+    analogWrite(G, r);
+    delay(tim);
+  }
+  for( r = 255; r >= 0; r-- ){ 
+    analogWrite(R, r);
+    analogWrite(G, r);
+    delay(tim);
+  }
+  delay(tim+500);
+
+    
+  for( r = 0; r < 255; r++ ){ 
+    analogWrite(G, r);
+    delay(tim);
+  }
+  for (r = 255; r >= 0; r--){ 
+    analogWrite(G, r);
+    delay(tim);
+  }
+  delay(tim+500);
+    
+  for( r = 0; r < 255; r++){ 
+    analogWrite(R, r);
+    analogWrite(B, r);
+    delay(tim);
+  }
+  for( r = 255; r >= 0; r--){ 
+    analogWrite(R, r);
+    analogWrite(B, r);
+    delay(tim);
+  }
+  delay(tim+500);
+    
+  for( r = 0; r < 255; r++ ){ 
+    analogWrite(G, r);
+    analogWrite(B, r);
+    delay(tim);
+  }
+  for( r = 255; r >= 0; r--){ 
+    analogWrite(G, r);
+    analogWrite(B, r);
+    delay(tim);
+  }
+  delay(tim+500);
+}
+
+void prog_2(){
+  int r,b,g;
+
+  digitalWrite(R, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(B, LOW);
+  delay(300);
+
+  // Синий
+  for( b = 0; b <= 255; b++ ){ 
+    analogWrite(B, b);
+    delay(tim);
+  } 
+
+  // Фиолетовый
+  for( r = 0; r <= 255; r++ ){ 
+    analogWrite(R, r);
+    delay(tim);
+  }
   
-  int i=0;
-  for(; i<255; ++i){
-     analogWrite( led, i);
-     delay(DELAY);
+  // Красный
+  for( b = 255; b >= 0; b-- ){ 
+    analogWrite(B, b);
+    delay(tim);
+  } 
+  // Желтый
+  for( g = 0; g <= 255; g++ ){ 
+    analogWrite(G, g);
+    delay(tim);
+  } 
+  // Зеленый
+  for( r = 255; r => 0; r-- ){ 
+    analogWrite(R, r);
+    delay(tim);
+  } 
+  // Бирюзовый
+  for( b = 0; b <= 255; b++ ){ 
+    analogWrite(B, b);
+    delay(tim);
+  } 
+  // Голубой
+  for( g = 255; g >= 0; g-- ){ 
+    analogWrite(G, g);
+    delay(tim);
   }
-  for(;i>0;--i){
-    analogWrite( led, i);
-    delay(DELAY);
+}
+
+void prog_3(){
+  byte old = 0;
+
+  digitalWrite(R, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(B, LOW);
+  delay(300);
+
+
+  for( byte curr=1; curr<7; curr++ ){
+
+    for(int level=0; level<=255; level++){ 
+      // Red 
+      if( (old&1)==0 && (curr&1) ){
+        analogWrite(R, level);
+      } 
+      if( old&1 && (curr&1)==0 ){
+        analogWrite(R, 255-level);
+      }
+
+      // Green 
+      if( (old&2)==0 && (curr&2) ){
+        analogWrite(G, level);
+      }
+      if( old&2 && (curr&2)==0 ){
+        analogWrite(G, 255-level);
+      }
+
+      // Blue
+      if( (old&4)==0 && (curr&4) ){
+        analogWrite(B, level);
+      }
+      if( old&4 && (curr&4)==0 ){
+        analogWrite(B, 255-level);
+      }
+      delay(5);
+    }
+
+    old = curr;
   }
-  analogWrite(led,0);
+}
+
+
+void prog_4(){
+  digitalWrite(R, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(B, LOW);
+  delay(300);
+
+  for(byte i=1;i<7;i++){
+    for(byte j=0;j<3;j++){
+      if( i & 1 )
+        digitalWrite(R, HIGH);
+      if( i & 2 )
+        digitalWrite(G, HIGH);
+      if( i & 4 )
+        digitalWrite(B, HIGH);
+          
+      delay(tim2);
+  
+      if( i & 1 )
+        digitalWrite(R, LOW);
+      if( i & 2 )
+        digitalWrite(G, LOW);
+      if( i & 4 )
+        digitalWrite(B, LOW);
+      delay(tim2);
+    }
+  }
+}
+
+
+
+
+void loop() {
+ // change_prog();
+
+  if( prog==0 || prog==1)  prog_1();
+  if( prog==0 || prog==2)  prog_2();
+  if( prog==0 || prog==3)  prog_3();
+  if( prog==0 || prog==4)  prog_4();
 }
